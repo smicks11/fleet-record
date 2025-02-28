a818b874edb34d4b24fde2e47df09a9d04ac743e
Smicks <great@greats-MacBook-Pro.local>
2025-02-28 10:02:02 +0100

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fleet_app/app/controller/device_info_controller.dart';
 import 'package:fleet_app/app/controller/global_controller.dart';
 import 'package:fleet_app/app/modules/car_portal/controllers/car_portal_controller.dart';
 import 'package:fleet_app/app/modules/home/controllers/home_controller.dart';
 import 'package:fleet_app/app/modules/login/controllers/login_controller.dart';
+import 'package:fleet_app/firebase_options.dart';
 import 'package:fleet_app/fleet.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   registerControllers();
   runApp(FleetApp());
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import firebase_core
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
@@ -13,6 +14,7 @@ import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
